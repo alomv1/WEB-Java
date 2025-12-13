e3900c14d2f597e53f6036cfdd70f8310c128441
vlad <voytkiv.vladislav@lll.kpi.ua>
2025-12-14 01:47:08 +0200

    feature: Add JPA Entities and Repositories

--- src/main/java/com/weblabs/webjava/domain/Order.java
+++ src/main/java/com/weblabs/webjava/domain/Order.java
@@ -1,5 +1,6 @@
 package com.weblabs.webjava.domain;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,14 +10,22 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
+@Entity
+@Table(name = "customer_orders")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
     private UUID userId;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> items;
+
     private String status;
     private LocalDateTime createdAt;
 }
--- src/main/java/com/weblabs/webjava/domain/OrderItem.java
+++ src/main/java/com/weblabs/webjava/domain/OrderItem.java
@@ -1,14 +1,30 @@
 package com.weblabs.webjava.domain;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.UUID;
+
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
 public class OrderItem {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
     private Product product;
+
     private int quantity;
     private double price;
 
